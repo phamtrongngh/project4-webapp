94014a0441c1306f5acc1061c709fbbc385381e1
Ben Alpha <benalpha1105@gmail.com>
2020-06-22 06:45:18 +0700

    remove some import file UserMB

--- src/main/java/MB/UserMB.java
+++ src/main/java/MB/UserMB.java
@@ -8,18 +8,12 @@ package MB;
 import com.sun.jersey.api.client.WebResource.Builder;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 import entities.User;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
-import java.util.logging.Logger;
 import javafx.scene.media.Media;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -35,14 +29,13 @@ import org.codehaus.jackson.map.ObjectMapper;
 @RequestScoped
 public class UserMB {
 
-    public List<User> getUsers() throws MalformedURLException, IOException {
+    public List<User> getUsers() throws IOException {
         ClientConfig clientConfig = new DefaultClientConfig();
         Client client = Client.create(clientConfig);
         WebResource webResource = client.resource("http://localhost:9032/user");
-        Builder builder = webResource.accept(MediaType.APPLICATION_JSON) //
+        Builder builder = webResource.accept(MediaType.APPLICATION_JSON)
                 .header("content-type", MediaType.APPLICATION_JSON);
         ClientResponse response = builder.get(ClientResponse.class);
-        
         if (response.getStatus() != 200) {
             System.out.println("Failed with HTTP Error code: " + response.getStatus());
             String error = response.getEntity(String.class);
